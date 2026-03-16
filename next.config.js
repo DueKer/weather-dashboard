ab9fb0c4868f6c3a568681fa1fecc3c5a6dc8068
luoye <1216504560@qq.com>
2026-03-16 17:33:14 +0800

    Add next.config.js file with Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
